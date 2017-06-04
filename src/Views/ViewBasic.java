d7fb397b21bfcee3357731360e9426a477ff6038
helenetran3 <tr.helene@hotmail.com>
2017-06-04 19:47:09 +0200

    Add buttons, I'm coding the background

--- src/Views/ViewBasic.java
+++ src/Views/ViewBasic.java
@@ -6,13 +6,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class ViewBasic extends JFrame implements ActionListener{
 
     JPanel corps = new JPanel();
     JPanel up = new JPanel();
     JLabel label = new JLabel();
-    JLabel labelUp = new JLabel();
+    //JLabel labelUp = new JLabel();
 
     Keyboard keyboard = new Keyboard();
     Controller controller;
@@ -26,19 +29,39 @@ public class ViewBasic extends JFrame implements ActionListener{
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JToolBar toolBar = new JToolBar();
+        this.getContentPane().add(toolBar, BorderLayout.NORTH);
+
+        JButton btnMenu = new JButton(new BtnMenuAction());
+        toolBar.add(btnMenu);
+
+        JSeparator separator = new JSeparator();
+        toolBar.add(separator);
+
+        JLabel lblThemeLabel = new JLabel("Theme");
+        toolBar.add(lblThemeLabel);
+
+        JSeparator separator_1 = new JSeparator();
+        toolBar.add(separator_1);
+
+        JButton btnQuit = new JButton(new BtnQuitAction());
+        toolBar.add(btnQuit);
+
         for (int i = 0 ; i<25 ; i++){
             keyboard.getButtonAlphabet().get(i).addActionListener(this);
         }
         corps.setBackground(Color.yellow);
-        up.setBackground(Color.cyan);
+        //this.setContentPane(new ImagePanel());
 
-        labelUp.setText("THEME : ");
+        //up.setBackground(Color.cyan);
+
+        //labelUp.setText("THEME : ");
 
         this.add(corps, BorderLayout.CENTER);
         this.add(keyboard, BorderLayout.SOUTH);
-        this.add(up, BorderLayout.NORTH);
+        //this.add(up, BorderLayout.NORTH);
 
-        up.add(labelUp, BorderLayout.CENTER);
+        //up.add(labelUp, BorderLayout.CENTER);
         corps.add(label, BorderLayout.EAST);
     }
 
@@ -47,6 +70,45 @@ public class ViewBasic extends JFrame implements ActionListener{
         //this.controller.run(button.getText().charAt(0));
         this.label.setText(button.getText());
     }
+    public class BtnMenuAction extends AbstractAction {
+        public BtnMenuAction(){
+            putValue(SMALL_ICON, new ImageIcon(ViewBasic.class.getResource("/Icons/home16.png")));
+            putValue(LARGE_ICON_KEY, new ImageIcon(ViewBasic.class.getResource("/Icons/home32.png")));
+            putValue(NAME, "Menu");
+            putValue(SHORT_DESCRIPTION, "Return to the menu");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //retour au menu
+        }
+    }
+
+    public class BtnQuitAction extends AbstractAction {
+        public BtnQuitAction(){
+            putValue(SMALL_ICON, new ImageIcon(ViewBasic.class.getResource("/Icons/quit16.png")));
+            putValue(LARGE_ICON_KEY, new ImageIcon(ViewBasic.class.getResource("/Icons/quit32.png")));
+            putValue(NAME, "Quit");
+            putValue(SHORT_DESCRIPTION, "Quit the game");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //quitter
+        }
+    }
+
+//    public class ImagePanel extends JPanel {
+//
+//        public void paintComponent(Graphics g) {
+//            try {
+//                Image img = ImageIO.read(new File("/Icons/background.jpg"));
+//                g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 
     public static void main(String[] args){
         JFrame t = new ViewBasic();
