a96b550d0a4e3bc883f0549b1bb36fea9f7fa863
helenetran3 <tr.helene@hotmail.com>
2017-06-07 09:08:41 +0200

    A better structured window for Menu

--- src/Views/ViewMenu.java
+++ src/Views/ViewMenu.java
@@ -6,9 +6,11 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
 
 /**
  * Created by helene on 06/06/17.
@@ -17,13 +19,10 @@ public class ViewMenu extends JFrame {
     Menu menu;
 
     JPanel corps = new JPanel();
+    JPanel panelBtn = new JPanel();
 
-    JLabel lblThemeLabel = new JLabel();
-    PanelImage pan = new PanelImage();
+    panelGraphics pan = new panelGraphics();
 
-    JToolBar toolBarNorth = new JToolBar();
-    JToolBar toolBarSouth = new JToolBar();
-    JSeparator separator = new JSeparator();
     JButton btnSubmit = new JButton(new BtnSubmitAction());
 
     public ViewMenu(){
@@ -35,17 +34,20 @@ public class ViewMenu extends JFrame {
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        lblThemeLabel.setText("Welcome to the Hangman game made for Lubra's school!");
-        this.getContentPane().add(toolBarNorth, BorderLayout.NORTH);
-        toolBarNorth.add(separator);
-        toolBarNorth.add(lblThemeLabel);
+//        corps.setLayout(new BorderLayout());
+//        corps.add(pan, BorderLayout.CENTER);
+//        corps.add(btnSubmit,BorderLayout.CENTER);
+//        this.add(corps,BorderLayout.CENTER);
 
         corps.setLayout(new BorderLayout());
-        corps.add(pan, BorderLayout.CENTER);
-
-        this.getContentPane().add(toolBarSouth, BorderLayout.SOUTH);
-        toolBarSouth.add(separator);
-        toolBarSouth.add(btnSubmit);
+        //Add the background
+        corps.add(pan);
+        //Add buttons
+        btnSubmit.setSize(50,50);
+        panelBtn.setLayout(new BorderLayout());
+        panelBtn.add(btnSubmit, BorderLayout.CENTER);
+        corps.add(panelBtn, BorderLayout.SOUTH);
+        this.setContentPane(corps);
     }
 
     public class BtnSubmitAction extends AbstractAction {
@@ -62,20 +64,25 @@ public class ViewMenu extends JFrame {
         }
     }
 
-    public class PanelImage extends JPanel {
-        private String path;
-
-        public PanelImage(){
+    public class panelGraphics extends JPanel {
+        public panelGraphics(){
             super();
         }
 
         public void paintComponent(Graphics g) {
             try {
+                //Background
                 FileInputStream input = new FileInputStream(new File("src/Icons/background.jpg"));
-
                 Image img = ImageIO.read(input);
-
                 g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+                
+                //Welcoming text
+                Font font = new Font("Courier",Font.BOLD,12);
+                g.setFont(font);
+                g.setColor(Color.black);
+                g.drawString("Welcome to the Hangman game made for Lubra's school !",this.getWidth()/2-200,50);
+
+                //Button
 
             }
             catch (IOException e) {
